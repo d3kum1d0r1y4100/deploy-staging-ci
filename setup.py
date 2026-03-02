7a551dba2ec432b57bdf1128c75f4720d39bfa92
gskckrn928262 <gskckrn928262@users.noreply.github.com>
2026-03-02 13:21:38 +0800

    Add python_requires constraint and .gitignore

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 setup(
     name='triage-test-env',
     version='1.0.0',
+    python_requires='>=3.12',
     py_modules=['app', 'deploy'],
     entry_points={
         'console_scripts': ['triage-test-env=app:main'],
